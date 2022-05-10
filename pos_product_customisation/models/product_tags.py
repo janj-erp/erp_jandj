9bed8eb7469171f72ad9f807bb4a506806629ff5
biswajeet <biswajeet@primemindsconsulting.com>
2022-05-10 16:37:46 +0530

    product changes

--- pos_product_customisation/models/product_tags.py
+++ pos_product_customisation/models/product_tags.py
@@ -16,11 +16,17 @@ class ProductTemplate(models.Model):
         'Item Code', compute='_compute_default_code',
         inverse='_set_default_code', store=True, required=True)
 
+
     @api.constrains('default_code')
     def sync_default_code(self):
         for rec in self.product_variant_ids:
             if not rec.default_code:
                 rec.default_code = self.default_code
+        # if self._name == 'product.template':
+        #     if self.default_code:
+        #         count = len(self.search([('default_code', '=', self.default_code)]))
+        #         if count > 1:
+        #             raise ValidationError(_('Cannot Set Duplicate Item Code'))
 
     def update_all_variant_codes(self):
         for product in self:
@@ -33,6 +39,10 @@ class ProductTemplate(models.Model):
     _inherit = 'product.product'
 
     default_code = fields.Char('Item Code', index=False, required=True)
+
+    @api.onchange('default_code')
+    def _onchange_default_code(self):
+        return
 
 
 
