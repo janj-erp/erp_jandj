b3f4add2ce20b6ac3c659b062cf02cc35afa098f
pmcplcj <97661777+pmcplcj@users.noreply.github.com>
2022-06-06 21:40:29 +0530

    New field bank name
    New field bank name

--- pos_product_customisation/models/product_tags.py
+++ pos_product_customisation/models/product_tags.py
@@ -77,10 +77,10 @@ class Product(models.Model):
     
  ##Move to new module later
 
-# class ResBankIn(models.Model):
-#     _inherit = 'res.bank'
+class ResBankIn(models.Model):
+    _inherit = 'res.bank'
 
-#     branch_name = fields.Char('Branch Name')
+    branch_name = fields.Char('Branch Name')
     
 #     def name_get(self):
 #         return [(rec.id, f"{rec.name} / {rec.branch_name}") for rec in self]
