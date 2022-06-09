96759de2e4ce12901b1b231053f52a68b1b68629
CJ Rohan <rohan@primemindsconsulting.com>
2022-06-09 16:44:02 +0530

    Add 03 j_and_j and pay_summary modules

---
+++ j_and_j_payroll/wizards/bulk_payslips.py
@@ -0,0 +1,61 @@
+from odoo import api, fields, models,_
+from datetime import date, datetime ,timedelta
+from odoo.tools.misc import format_date
+
+
+
+class BulkPayslips(models.TransientModel):
+    _name = 'bulk.payslip.wizard'
+    _description = 'Wizard to generate bulk payslips'
+
+    structure_id = fields.Many2one('hr.payroll.structure', "Structure")
+    date_from = fields.Date("Date From")
+    date_to = fields.Date("Date To")
+
+
+
+    def create_bulk_payslips(self):
+        payslip = self.env['hr.payslip']
+        structure_id = self.structure_id.id
+        employee = self.env['hr.employee'].search([('structure','=',structure_id)])
+        date_from = self.date_from
+        date_to = self.date_to
+        batch_obj = self.env['hr.payslip.run']
+        week = date_from.strftime("%W")
+        if 'Weekly' in self.structure_id.name:
+            batch = batch_obj.create({'name':
+                                      '%(week)s- Week- From %(date_start)s - To  %(date_end)s' % {
+                                          'week': week,
+                                          'date_start': format_date(self.env, date_from, date_format="d MMM y"),
+                                          'date_end': format_date(self.env, date_to, date_format="d MMM y"),
+                                      },
+                                      'date_start': date_from,
+                                      'date_end': date_to,
+                                      })
+        else:
+            batch = batch_obj.create({'name': format_date(self.env, date_from, date_format="MMMM y"),
+                                      'date_start': date_from,
+                                      'date_end': date_to,
+                                      })
+        for emp in employee:
+            if emp.contract_id :
+                vals = {'employee_id': emp.id,
+                        'struct_id': emp.structure.id,
+                        'date_from': date_from,
+                        'date_to': date_to,
+                        'contract_id':emp.contract_id.id,
+                        'name': '%(payslip_name)s - %(employee_name)s - %(dates)s' % {
+                            'payslip_name': 'Salary Slip',
+                            'employee_name': emp.name,
+                            'dates': format_date(self.env, date_from, date_format="MMMM y")},
+                        'payslip_run_id': batch.id,
+                        }
+                new_rec = payslip.create(vals)
+                new_rec.calculate_employee_working_hours_in_current_week()
+                new_rec.get_total_number_of_days()
+                new_rec.get_total_unpaid_of_days()
+                new_rec.compute_sheet()
+
+
+
+
---
+++ payroll_summary/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name' : 'J & J Payroll Summary',
+    'version' : '15.0.0.3 june-7',
+    'summary': 'J & J Payroll Summary',
+    'sequence': 10,
+    'description': """
+    Payroll Summary Report for monthly/weekly 
+    """,
+    'author':'Hema',
+    'category': 'HR Payroll',
+    'website': 'https://www.odoo.com/',
+    'depends' : ['base','hr_payroll','report_xlsx','j_and_j_payroll'],
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/payroll_summary.xml',
+        'wizard/bank_transfer_summary.xml',
+        'views/payslip.xml',
+        'report/report.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
