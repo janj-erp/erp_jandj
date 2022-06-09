7a3fe56228faf85687b42f333a3279b0d443abf0
CJ Rohan <rohan@primemindsconsulting.com>
2022-06-09 15:11:09 +0530

    Remove all 004

--- jnj_reports/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import hr_employee_inherit
-from . import employee_experience_details
-from . import employee_qualification
-from . import termination_reason
--- time_office_attendance_integration/models/res_company_inherit.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class Company(models.Model):
-    _inherit = 'res.company'
-
-    url = fields.Char('URL')
-    corporate_id = fields.Char('Corporate Id')
-    username = fields.Char('Username')
-    password = fields.Char('Password')
