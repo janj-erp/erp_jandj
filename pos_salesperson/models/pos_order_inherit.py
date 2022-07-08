abee6c1071d33e696e0e3554ea00a0d35a10622c
CJ Rohan <rohan@primemindsconsulting.com>
2022-07-08 14:16:28 +0530

    Added active for orders

--- pos_salesperson/models/pos_order_inherit.py
+++ pos_salesperson/models/pos_order_inherit.py
@@ -5,6 +5,7 @@ class PosOrder(models.Model):
     _inherit = 'pos.order'
 
     salesperson_id = fields.Many2one('hr.employee', string='Salesperson')
+    active = fields.Boolean('Active', default=True, groups='base.group_system')
 
     @api.model
     def _order_fields(self, ui_order):
